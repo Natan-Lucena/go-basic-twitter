79fd37cc8bb18b4e0c6d6968aa6aaec6d167a9fd
Natan Lucena <zerogamerptbr@gmail.com>
2024-07-02 11:15:42 -0300

    :sparkles: feat: add ROUTE to delete tweets

--- controllers/TweetController.go
+++ controllers/TweetController.go
@@ -29,3 +29,14 @@ func (controller *tweetController) Create(ctx *gin.Context) {
 	controller.tweets = append(controller.tweets, *tweet)
 	ctx.JSON(http.StatusOK, tweet)
 }
+func (controller *tweetController) DeleteById(ctx *gin.Context) {
+	id:= ctx.Param("id")
+	for i, tweet := range controller.tweets{
+		if tweet.ID == id{
+			controller.tweets = append(controller.tweets[0:i], controller.tweets[i+1:]...)
+			ctx.JSON(http.StatusOK, gin.H{"message": "Tweet deleted"})
+			ctx.Status(http.StatusOK)
+			return
+	}}
+	ctx.JSON(http.StatusNotFound, gin.H{"error": "Tweet not found"})
+}
