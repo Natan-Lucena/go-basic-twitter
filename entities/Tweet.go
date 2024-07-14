20e4d39f1013192649cc4cb561ca939d1b148b9f
Natan Lucena <zerogamerptbr@gmail.com>
2024-07-14 20:07:48 -0300

    :sparkles: feat: add relation in tweet entity with user entity

--- entities/Tweet.go
+++ entities/Tweet.go
@@ -5,6 +5,8 @@ import "github.com/pborman/uuid"
 type Tweet struct {
 	ID string `gorm:"primary_key" json:"id"`
 	Description string `json:"description"`
+	UserID      string `json:"userId"`
+	User        User   `gorm:"foreignkey:UserID"`
 }
 
 func NewTweet() *Tweet {
