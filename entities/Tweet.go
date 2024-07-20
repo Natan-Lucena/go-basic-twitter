080ff66ab742751aeda6d411df36a22d617650b7
Natan Lucena <zerogamerptbr@gmail.com>
2024-07-20 13:55:39 -0300

    :sparkles: feat: add reply tweet field

--- entities/Tweet.go
+++ entities/Tweet.go
@@ -3,10 +3,12 @@ package entities
 import "github.com/pborman/uuid"
 
 type Tweet struct {
-	ID string `gorm:"primary_key" json:"id"`
-	Description string `json:"description"`
-	UserID      string `type:"varchar(191);not null" json:"userId"`
-	User        User   `gorm:"foreignkey:UserID"`
+	ID           string `gorm:"primary_key" json:"id"`
+	Description  string `json:"description"`
+	UserID       string `type:"varchar(191);not null" json:"userId"`
+	User         User   `gorm:"foreignkey:UserID"`
+	ReplyToTweet *string `json:"replyToTweet,omitempty"`
+	ReplyTo      *Tweet  `gorm:"foreignkey:ReplyToTweet"`
 }
 
 func NewTweet() *Tweet {
