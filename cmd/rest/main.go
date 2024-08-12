5e7ddda07feb085fa9824bb7cd27265db2d8337d
Natan Lucena <zerogamerptbr@gmail.com>
2024-08-12 17:21:47 -0300

    :lock: security: using cors middleware

--- cmd/rest/main.go
+++ cmd/rest/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	rest "crud-go/cmd/rest/routes"
+	"crud-go/internal/controllers/middlewares"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	app := gin.Default()
+	app.Use(middlewares.CORSMiddleware())
 	rest.AppRoutes(app)
 	app.Run("localhost:3001")
 }
