8d5fbd87500ef5d7e7ebf90f945d81a4c66eb463
Natan Lucena <zerogamerptbr@gmail.com>
2024-08-12 17:21:24 -0300

    :bug: fix: fix .env correct location

--- pkg/gorm/init_db.go
+++ pkg/gorm/init_db.go
@@ -11,7 +11,7 @@ import (
 )
 
 func InitDB() (*gorm.DB, error) {
-	err := godotenv.Load("../../.env")
+	err := godotenv.Load()
     if err != nil {
         log.Fatalf("Erro ao carregar arquivo .env: %v", err)
     }
